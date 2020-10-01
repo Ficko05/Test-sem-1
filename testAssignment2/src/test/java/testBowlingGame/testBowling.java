4afd7813d3b919bb577d712681ea854eec1e2d65
Ficko05 <filip-f@hotmail.com>
2020-10-01 12:48:26 +0200

    adding <properties>

---
+++ testAssignment2/src/test/java/testBowlingGame/testBowling.java
@@ -0,0 +1,21 @@
+package testBowlingGame;
+
+import org.testng.annotations.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class testBowling {
+
+    @Test
+    public void test() {
+        // Arrange
+
+        // Act
+
+        // Assert
+
+    }
+}
